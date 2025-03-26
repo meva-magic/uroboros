a2a498be8bc2d7389da64ce4713fb088a986160b
saharov-dev <saharov.dev@gmail.com>
2025-03-26 23:14:35 +0300

    >_<

--- uroboros/Assets/Scripts/Rotation.cs
+++ uroboros/Assets/Scripts/Rotation.cs
@@ -1,14 +1,31 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Rotation : MonoBehaviour
 {
-    [SerializeField] private float turnSpeed;
+    [SerializeField] private float turnSpeed = 100f;
 
     private float direction;
 
     private void FixedUpdate()
     {
-        direction = Input.GetAxisRaw("Horizontal") * -1;
+        direction = 0f;
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            {
+                if (touch.position.x < Screen.width / 2)
+                {
+                    direction = 1f;
+                }
+                else
+                {
+                    direction = -1f;
+                }
+            }
+        }
 
         transform.Rotate(Vector3.forward * turnSpeed * direction * Time.deltaTime);
     }
